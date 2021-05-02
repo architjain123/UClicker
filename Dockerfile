98a3d4d4e62aee3137088a246ef07247e3c89d5b
Evan Chopra <evanchopra7698@gmail.com>
2021-05-01 23:53:23 -0700

    Fixed dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM python:3.6-stretch
 WORKDIR /project
 ADD . /project
 RUN pip3 install -r requirements.txt
-EXPOSE 5001
-CMD ["python","app.py"]
+EXPOSE 8080
+CMD ["python3","app.py"]
--- app.py
+++ app.py
@@ -12,4 +12,4 @@ app.register_blueprint(login_routes_blueprint,url_prefix="/account")
 cors = CORS(app)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
